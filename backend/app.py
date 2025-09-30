4bf47d9b951856d94e92d688edcbe905b695cf26
Yogesh-A06 <lokeshanbuselvan@gmail.com>
2025-09-30 07:00:17 +0530

    p7

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://eudg.netlify.app"],
+    allow_origins=["http://localhost:3000","https://edu-glow-brown.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
